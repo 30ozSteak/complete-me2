d186532595d7fdd64e6acec7969c4852be4d8164
Nick Dambrosio <ndambro303@gmail.com>
2018-08-29 17:16:43 -0600

    Add dictionary test, takes 870ms though

--- test/trie-test.js
+++ test/trie-test.js
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 import Trie from '../lib/Trie.js';
 import Node from '../lib/Node.js';
+import fs from 'fs';
 const text = '/usr/share/dict/words';
 const dictionary = fs.readFileSync(text).toString().trim().split('\n');
 
@@ -28,14 +29,18 @@ describe('Trie', () => {
     expect(trie.root.children).to.deep.equal({});
   });
 
-
   describe('Insert', () => {
-    it('should insert a new node into the trie', () => {
+    it ('should insert a new node into the trie', () => {
       let data = 'warby';
   
       trie.insert(data);
   
       expect(Object.keys(trie.root.children)[0]).to.equal('w');
+    });
+
+    it ('should get words from the built-in dictionary and put them into the trie', () => {
+      trie.populate(dictionary);
+      expect(trie.count).to.deep.equal(235886);
     });
   });
 
